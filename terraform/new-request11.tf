cfd941e5832f961e268dd5cbf96866d00a4d86b2
SaiDev143 <137700172+SaiDev143@users.noreply.github.com>
2025-05-24 12:45:33 +0400

    Created file terraform/new-request11.tf

---
+++ terraform/new-request11.tf
@@ -0,0 +1,30 @@
+module "new-request10" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "newaccount98@yopmail.com"
+    AccountName               = "new accunt"
+    ManagedOrganizationalUnit = "Test-OU" 
+    SSOUserEmail              = "newaccount98@yopmail.com"
+    SSOUserFirstName          = "automation"
+    SSOUserLastName           = "account"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "Shanmukh"
+    "ABC:Division"    = "ENT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+    run_create_pipeline = "false"
+  }
+
+  account_customizations_name = "tobeseen"
+}
